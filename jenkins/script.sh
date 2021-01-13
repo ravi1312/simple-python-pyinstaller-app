157e0502b9c464caeacd4770a4c589109e48c6ec
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2021-01-13 08:48:01 +0530

    Update script.sh

--- jenkins/script.sh
+++ jenkins/script.sh
@@ -1,5 +1,5 @@
 git_repo=$1
 #a= `echo $git_repo | cut -d "/" -f5 | cut -d "." -f1`
 #echo $a
-a=`echo $git_repo | cut -d "/" -f5 | cut -d "." -f1
+a=`echo $git_repo | cut -d "/" -f5 | cut -d "." -f1`
 echo $a`
