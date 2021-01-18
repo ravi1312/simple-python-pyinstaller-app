ba56c2fea0a02e65197c126fab63b8c4d5a23f27
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2021-01-18 10:36:55 +0530

    Update add2vals.py

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -1,6 +1,7 @@
 '''
 A simple command line tool that takes 2 values and adds them together using
 the calc.py library's 'add2' function.
+this is a sample example we have done so far.
 '''
 
 import sys
